dd4db3a4f967c81ff9aa01bd59e4d712eb5cc9b7
Spareheads <94579408+Spareheads@users.noreply.github.com>
2021-11-26 22:55:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,23 +9,23 @@ menu_list_array = ["Veg Margherita Pizza",
         var htmldata="";
         menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
+            htmldata=htmldata+ menu_list_array[i] + '<br>';
         }
         document.getElementById("display_menu").innerHTML = htmldata;
         //give the appropriate id name as display_menu 
     }
 
     function add_item(){
-        var htmldata="";
-        menu_list_array.sort();
-		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">';
         var item=document.getElementById("add_item").value;
-        // use the sort function as - menu_list_array.sort();
+        menu_list_array.sort();
+        
         htmldata="";
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
         }
-         document.getElementById("display_added_menu").innerHTML = htmldata;
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
 		
     }
 
@@ -34,3 +34,4 @@ function add_top(){
     menu_list_array.push(item);
 	add_item();
 }
+
